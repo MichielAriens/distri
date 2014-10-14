62c60b6c726af2c439eb0ec68115193df97099e2
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-10-14 11:26:11 +0200

    session

--- task2/CarRental-ejb/src/java/session/CarRentalSession.java
+++ task2/CarRental-ejb/src/java/session/CarRentalSession.java
@@ -2,16 +2,55 @@ package session;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateful;
+import rental.CarRentalCompany;
+import rental.Quote;
 import rental.RentalStore;
+import rental.ReservationConstraints;
+import rental.ReservationException;
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
+    
+    private Set<Quote> quotes = new HashSet<Quote>();
+    private String clientName = "";
 
     @Override
     public Set<String> getAllRentalCompanies() {
         return new HashSet<String>(RentalStore.getRentals().keySet());
     }
+
+    @Override
+    public Quote createQuote(ReservationConstraints cons, String rentalCompany) throws ReservationException{
+        CarRentalCompany crc = RentalStore.getRentals().get(rentalCompany);
+        Quote quote = crc.createQuote(cons, clientName);
+        quotes.add(quote);
+        return quote;
+    }
+
+    @Override
+    public Set<Quote> getCurrentQuotes() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void confirmQuotes() throws ReservationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setClientName(String name) {
+        this.clientName = name;
+    }
+
+    @Override
+    public Quote createQuote(ReservationConstraints cons) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 
     
     
--- task2/CarRental-lib/src/session/CarRentalSessionRemote.java
+++ task2/CarRental-lib/src/session/CarRentalSessionRemote.java
@@ -4,14 +4,21 @@ import java.util.Set;
 import javax.ejb.Remote;
 import rental.Quote;
 import rental.ReservationConstraints;
+import rental.ReservationException;
 
 @Remote
 public interface CarRentalSessionRemote {
+    
+    void setClientName(String name);
 
     Set<String> getAllRentalCompanies();
     
-    Quote createQuote(ReservationConstraints cons);
+    Quote createQuote(ReservationConstraints cons, String rentalCompany) throws ReservationException;
     
     Set<Quote> getCurrentQuotes();
+    
+    void confirmQuotes(Set<Quote> quotes) throws ReservationException;
+    
+    
     
 }
