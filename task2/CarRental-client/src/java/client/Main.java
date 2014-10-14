5db0510f9302e0af56038b5af8ad9088b37cb11c
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-10-14 11:03:50 +0200

    gitignore & start 2

--- task2/CarRental-client/src/java/client/Main.java
+++ task2/CarRental-client/src/java/client/Main.java
@@ -7,10 +7,14 @@ public class Main {
     
     @EJB
     static CarRentalSessionRemote session;
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("found rental companies: "+session.getAllRentalCompanies());
     }
+    
+    
 }
--- task2/CarRental-lib/src/session/CarRentalSessionRemote.java
+++ task2/CarRental-lib/src/session/CarRentalSessionRemote.java
@@ -2,10 +2,16 @@ package session;
 
 import java.util.Set;
 import javax.ejb.Remote;
+import rental.Quote;
+import rental.ReservationConstraints;
 
 @Remote
 public interface CarRentalSessionRemote {
 
     Set<String> getAllRentalCompanies();
+    
+    Quote createQuote(ReservationConstraints cons);
+    
+    Set<Quote> getCurrentQuotes();
     
 }
