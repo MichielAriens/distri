3183ed180873a224042a40a2f6974f9280af4e2f
JulieWouters <Julie.wouters@student.kuleuven.be>
2014-11-12 14:35:00 +0100

    managersession

--- task4/CarRental-ejb/src/java/session/ManagerSession.java
+++ task4/CarRental-ejb/src/java/session/ManagerSession.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import rental.Car;
 import rental.CarRentalCompany;
 import rental.CarType;
@@ -13,10 +16,14 @@ import rental.Reservation;
 @Stateless
 public class ManagerSession implements ManagerSessionRemote {
     
+    @PersistenceContext
+    EntityManager em;
+    
     @Override
     public Set<CarType> getCarTypes(String company) {
         try {
-            return new HashSet<CarType>(RentalStore.getRental(company).getAllTypes());
+            CarRentalCompany crc = getCompany(company);
+            return new HashSet<CarType>(crc.getAllTypes());
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
             return null;
@@ -27,7 +34,8 @@ public class ManagerSession implements ManagerSessionRemote {
     public Set<Integer> getCarIds(String company, String type) {
         Set<Integer> out = new HashSet<Integer>();
         try {
-            for(Car c: RentalStore.getRental(company).getCars(type)){
+            CarRentalCompany crc = getCompany(company);
+            for(Car c: crc.getCars(type)){
                 out.add(c.getId());
             }
         } catch (IllegalArgumentException ex) {
@@ -40,7 +48,8 @@ public class ManagerSession implements ManagerSessionRemote {
     @Override
     public int getNumberOfReservations(String company, String type, int id) {
         try {
-            return RentalStore.getRental(company).getCar(id).getReservations().size();
+            CarRentalCompany crc = getCompany(company);
+            return crc.getCar(id).getReservations().size();
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
             return 0;
@@ -51,7 +60,8 @@ public class ManagerSession implements ManagerSessionRemote {
     public int getNumberOfReservations(String company, String type) {
         Set<Reservation> out = new HashSet<Reservation>();
         try {
-            for(Car c: RentalStore.getRental(company).getCars(type)){
+            CarRentalCompany crc = getCompany(company);
+            for(Car c: crc.getCars(type)){
                 out.addAll(c.getReservations());
             }
         } catch (IllegalArgumentException ex) {
@@ -60,12 +70,18 @@ public class ManagerSession implements ManagerSessionRemote {
         }
         return out.size();
     }
+    
+    public CarRentalCompany getCompany(String company){
+        Query query = em.createQuery("SELECT e FROM CarRentalCompany e WHERE e.name LIKE :compName").setParameter("compName",company);
+        return (CarRentalCompany) query.getResultList().get(0);
+    }
 
     @Override
     public int getNumberOfReservationsBy(String renter) {
         Set<Reservation> out = new HashSet<Reservation>();
-        for(CarRentalCompany crc : RentalStore.getRentals().values()) {
-            out.addAll(crc.getReservationsBy(renter));
+        Query query = em.createQuery("SELECT e FROM CarRentalCompany e");
+        for(Object crc : query.getResultList()) {
+            out.addAll(((CarRentalCompany)crc).getReservationsBy(renter));
         }
         return out.size();
     }
